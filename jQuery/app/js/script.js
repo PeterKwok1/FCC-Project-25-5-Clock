cf916ef7dd95af14dd91ec87a9eeeb66be734ae2
Peter Kwok <peter.kwok94@gmail.com>
2023-09-06 18:01:39 -0400

    script

--- jQuery/app/js/script.js
+++ jQuery/app/js/script.js
@@ -5,7 +5,7 @@
 // break run function after timer
 
 // to dos
-// generalize inc and dec variables and functions?
+// generalize inc and dec variables and functions
 // adjust code to use lengths arr. idk if going to use object or not. 
 // session label swap on countdown change
 // audio
@@ -23,7 +23,7 @@ let lengths = [ // name and order
         length: 5 * 60 * 1000 // 5 minutes
     }]
 let currentLength = 0
-let runningLength
+let runningLength = lengths[currentLength].length
 let isRunning = false
 
 // display 
@@ -44,24 +44,34 @@ function display(tag, length, padded) {
 
 // timer set
 const timeLeftTag = $('#time-left')
-display(timeLeftTag, lengths[0].length, true)
+display(timeLeftTag, runningLength, true) // initialize
 
 const sessionLengthTag = $('#session-length')
 const sessionIncTag = $('#session-increment')
 const sessionDecTag = $('#session-decrement')
 display(sessionLengthTag, lengths[0].length) // initialize
 sessionIncTag.click(() => {
-    if (lengths[0].length < 60 * 60 * 1000) { // greater than 0 and less than 60 minutes
-        lengths[0].length += 60 * 1000 // 1 minute
-        display(sessionLengthTag, lengths[0].length)
-        display(timeLeftTag, lengths[0].length, true)
+    if (!isRunning) {
+        if (lengths[0].length < 60 * 60 * 1000) { // less than 60 minutes
+            lengths[0].length += 60 * 1000 // 1 minute
+            display(sessionLengthTag, lengths[0].length)
+            if (currentLength === 0) {
+                runningLength = lengths[currentLength].length
+                display(timeLeftTag, runningLength, true)
+            }
+        }
     }
 })
 sessionDecTag.click(() => {
-    if (lengths[0].length > 0) { // greater than 0 and less than 60 minutes
-        lengths[0].length -= 60 * 1000 // 1 minute
-        display(sessionLengthTag, lengths[0].length)
-        display(timeLeftTag, lengths[0].length, true)
+    if (!isRunning) {
+        if (lengths[0].length > 60 * 1000) { // greater than 1 minute
+            lengths[0].length -= 60 * 1000 // 1 minute
+            display(sessionLengthTag, lengths[0].length)
+            if (currentLength === 0) {
+                runningLength = lengths[currentLength].length
+                display(timeLeftTag, lengths[0].length, true)
+            }
+        }
     }
 })
 
@@ -71,22 +81,36 @@ const breakIncTag = $('#break-increment')
 const breakDecTag = $('#break-decrement')
 display(breakLengthTag, lengths[1].length) // initialize 
 breakIncTag.click(() => {
-    if (lengths[1].length < 60 * 60 * 1000) { // greater than 0 and less than 60 minutes
-        lengths[1].length += 60 * 1000 // 1 minute
-        display(breakLengthTag, lengths[1].length)
+    if (!isRunning) {
+        if (lengths[1].length < 60 * 60 * 1000) { // less than 60 minutes
+            lengths[1].length += 60 * 1000 // 1 minute
+            display(breakLengthTag, lengths[1].length)
+            if (currentLength === 1) {
+                runningLength = lengths[currentLength].length
+                display(timeLeftTag, runningLength, true)
+            }
+
+        }
     }
 })
 breakDecTag.click(() => {
-    if (lengths[1].length > 0) { // greater than 0 and less than 60 minutes
-        lengths[1].length -= 60 * 1000 // 1 minute
-        display(breakLengthTag, lengths[1].length)
+    if (!isRunning) {
+        if (lengths[1].length > 60 * 1000) { // greater than 1 minute
+            lengths[1].length -= 60 * 1000 // 1 minute
+            display(breakLengthTag, lengths[1].length)
+            if (currentLength === 1) {
+                runningLength = lengths[currentLength].length
+                display(timeLeftTag, runningLength, true)
+            }
+        }
     }
 })
 
 // timer run
 function countDown() {
-    runningLength = lengths[currentLength].length
     const curCntDwn = setInterval(() => {
+        runningLength -= 1000 // 1 second
+        display(timeLeftTag, runningLength, true)
         if (runningLength === 0) { // end of current countdown 
             clearInterval(curCntDwn)
             if (currentLength < lengths.length - 1) { // alternator
@@ -94,20 +118,23 @@ function countDown() {
             } else {
                 currentLength = 0
             }
+            runningLength = lengths[currentLength].length
             countDown()
-        } else {
-            runningLength -= 1000 // 1 second
-            display(timeLeftTag, runningLength, true)
         }
     }, 1000)
     isRunning = curCntDwn
 }
 const startStopTag = $('#start_stop')
 startStopTag.click(() => {
-    countDown()
+    if (!isRunning) {
+        countDown()
+    } else {
+        clearInterval(isRunning)
+        isRunning = false
+    }
 })
 
-// reset
+// reset // reset is similar to a combination of session set and break set
 const resetTag = $('#reset')
 resetTag.click(() => {
     if (isRunning) {
@@ -115,8 +142,11 @@ resetTag.click(() => {
         isRunning = false
     }
     lengths[0].length = 25 * 60 * 1000
-    lengths[1].length = 5 * 60 * 1000
     display(sessionLengthTag, lengths[0].length)
+    lengths[1].length = 5 * 60 * 1000
     display(breakLengthTag, lengths[1].length)
-    display(timeLeftTag, lengths[0].length, true)
+    currentLength = 0
+    runningLength = lengths[currentLength].length
+    display(timeLeftTag, runningLength, true)
 })
+
